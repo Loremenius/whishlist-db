af192ee7aaccb8024355b67fffb56f6a33264cea
Marcellino Fernandez <munch2000@gmail.com>
2020-05-23 14:15:00 -0600

    fixed AuthorizeAction: parseInt parameter id

--- api/middleware/AuthorizeAction.js
+++ api/middleware/AuthorizeAction.js
@@ -1,11 +1,13 @@
 module.exports = (req, res, next) => {
     console.log('Authorizing user to edit list.');
+    console.log(req.token.id);
+    console.log(req.params.id);
 
-    if (req.token.id === req.params.id){
-        print('user is authorized to edit list')
+    if (req.token.id === parseInt(req.params.id)){
+        console.log('user is authorized to edit list')
         next()
     }else{
-        print('user is not allowed to edit list')
+        console.log('user is not allowed to edit list')
         res.status('500').json({message:`user is not allowed to edit list of user:${req.params.id}`})
     }
 }
