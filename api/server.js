eb1c719bacaa1ac8e7e2d3860c0eff0d086f4196
Marcellino Fernandez <munch2000@gmail.com>
2020-07-28 23:38:40 -0600

    added comments in server.js

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const user = require('./user/user-router');
 const gift = require('./user/gift-router')
 
+// imports middleware function to check for valid token from request header
 const ValidateToken = require('./middleware/ValidateToken');
 
 const server = express();
@@ -12,7 +13,10 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+// user routes uses '/api/user' as the begining to all of the routes under user-router.js
 server.use("/api/user", user);
+// user routes uses '/api/user/wishlist' as the begining to all of the routes under gift-router.js
+// all routes using '/api/user/wishlist' also use the ValidateToken Middleware
 server.use('/api/user/wishlist',ValidateToken, gift)
 
 
