995ab062d39101112e5e19b13d08099766ede54c
Marcellino Fernandez <munch2000@gmail.com>
2020-08-04 15:18:26 -0600

    added comments for login route

--- api/user/user-router.js
+++ api/user/user-router.js
@@ -4,16 +4,25 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 const userDb = require('./user-model')
-const validateLogin = require('../middleware/ValidateLogin');
+const ValidateLogin = require('../middleware/ValidateLogin');
 const ValidateRegister = require('../middleware/ValidateRegister');
 const FindUserByUsername = require('../middleware/FindUserByUsername');
-
-router.post('/login', validateLogin, (req, res)=>{
+// This route is used to login the user to the website.
+// uses ValidateLogin middleware to verify request body has correct information for login.
+router.post('/login', ValidateLogin, ( req, res ) => {
+    // takes username and makes all characters lowercase
     req.body.username = req.body.username.toLowerCase();
+    // uses SQL queries to find the user with given username.
     userDb.findByName(req.body.username)
-        .then(data=>{
+        // if the SQL query is successful
+        .then( data => {
+            // if there is data and the password in request body is the same to the password in the database.
+            // the password given is not hashed but the one in the database is. 
+            // uses bcrypt.compareSync function to compare the unhashed password and hashed password ( returns boolean )
             if( data && bcrypt.compareSync(req.body.password, data.password) ){
+                // uses signToken function to create a token. ( takes in data from SQL query )
                 const token = signToken(data);
+                // responds with object containing welcome message, object of logging in user's data, and the token
                 res.status(200).json({
                     message:`Welcome ${data.username}`,
                     user:{
@@ -24,16 +33,19 @@ router.post('/login', validateLogin, (req, res)=>{
                     },
                     token:token
                 });
+            // if there is no data or the passwords do not match
             }else{
-                res.status(401).json({message:'Invalid Credentials'})
+                // respond with error message saying the credentials are invalid.
+                res.status(401).json( { message:'Invalid Credentials' } );
             }
         })
+        // if there is an error retrieving from database: log error in console and send response back with error and errorMessage.
         .catch(error=>{
             console.log(error);
             res.status(500).json({
                 error:error,
                 message:'error logging in user'
-            })
+            });
         })
 })
 
