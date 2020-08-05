77047ca31f4e7cbffc1d868f8618a9801eb12808
Marcellino Fernandez <munch2000@gmail.com>
2020-08-05 16:09:20 -0600

    added comments for FindByUsername middleware

--- api/middleware/FindUserByUsername.js
+++ api/middleware/FindUserByUsername.js
@@ -1,9 +1,16 @@
 const db = require('../user/user-model');
+// this function is used to check to see if there is a username already in the database
+// if there is already a username in the database: respond saying username has been taken
+// if it is not in the database already, send them to next function.
 module.exports = (req, res, next) => {
-    console.log('checking for existing user in database');
-    db.findByName(req.body.username)
-        .then(data=>{
+    // console log to show function is starting.
+    console.log( 'checking for existing user in database' );
+    // uses SQL query to find user with given username
+    db.findByName( req.body.username )
+        .then( data => {
+            // if there is data returned respond with message saying username is taken.
             if (!!data) res.status(400).json({ message:'Username has already been taken' });
+            // if there is no data returned move to next function.
             else next();
         })
 }
