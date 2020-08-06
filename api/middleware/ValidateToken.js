ab2a217af0bd7585e55ebd8c2197d9f8743baf60
Marcellino Fernandez <munch2000@gmail.com>
2020-08-06 15:02:21 -0600

    added comments for ValidateToken middleware

--- api/middleware/ValidateToken.js
+++ api/middleware/ValidateToken.js
@@ -1,22 +1,31 @@
 const jwt = require("jsonwebtoken"); 
-
-module.exports = (req, res, next) => {
+// This function is used to verify that the request is using a valid token.
+module.exports = ( req, res, next ) => {
+  // pulls token from request header
   const { authorization } = req.headers;
+  // console log to show function is starting.
   console.log(`checking for valid token`);
-
-  if (authorization) {
+  // if authorization is not empty
+  if ( authorization ) {
+    // acquire secret used for tokens
     const secret = process.env.JWT_SECRET || "Santa's Little Helper";
-
-    jwt.verify(authorization, secret, function(error, decodedToken) {
-      if (error) {
+    // use verify function from jsonwebtoken to verify the token from request.
+    jwt.verify( authorization, secret, function(error, decodedToken ) {
+      // if there was an error validating the token
+      if ( error ) {
+        // respond with message saying the token is invalid.
         res.status(401).json({ message: "Invalid Token" });
+      // if the token is valid
       } else {
+        // add the decoded token to the request object 
         req.token = decodedToken;
-        
+        // send request to next function. 
         next();
       }
     });
+    // if there is no token
   } else {
+    // respond with message saying they must login again.
     res.status(400).json({ message: "Please login and try again" });
   }
 };
