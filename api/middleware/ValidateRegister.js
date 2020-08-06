cfbbc2774c651bfc0827b3f2f51e877452f0c61b
Marcellino Fernandez <munch2000@gmail.com>
2020-08-06 14:34:25 -0600

    added comments for ValidateRegister middleware

--- api/middleware/ValidateRegister.js
+++ api/middleware/ValidateRegister.js
@@ -1,16 +1,18 @@
+// This function is used for making sure the body of the register request has the correct values.
 module.exports = (req, res, next) => {
+    // console log to show function is starting.
     console.log('checking for valid user in req body');
-    console.log(req.body)
-
+    // if there is nothing in the request body
     if(Object.entries(req.body).length === 0){
-
-        res.status(400).json({ message: "missing user data in request body" });
-
-        }else if (!req.body.username || !req.body.password || !req.body.firstname || !req.body.lastname){
-
-        res.status(400).json({ message: "missing username, password, firstname, and lastname in request body" });
-
-        }else{
+            // respond with message sying there is missing data in request body.
+            res.status(400).json({ message: "missing user data in request body" });
+        // if username, password, firstname, or lastname is empty.
+        } else if ( !req.body.username || !req.body.password || !req.body.firstname || !req.body.lastname ) {
+            // respond with message saying missing username, password, firstname, or lastname. 
+            res.status(400).json({ message: "missing username, password, firstname, and lastname in request body" });
+        // if everything for register is correct.
+        } else {
+            // send request to next function.
             next();
         }
 }
