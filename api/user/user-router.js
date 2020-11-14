57f2711b26fbe0ebf041af3321b6aefae05a1d7b
Marcellino Fernandez <munch2000@gmail.com>
2020-11-13 17:20:34 -0700

    commented out delete user

--- api/user/user-router.js
+++ api/user/user-router.js
@@ -141,31 +141,31 @@ router.get('/families/:name', ( req, res ) => {
         })
 });
 
-router.delete('/:username', (req, res)=>{
-    userDb.removeUser( req.params.username )
-    // if the SQL query is successful 
-    .then(data=>{
-        // if the length of the array from the SQL query is 0
-        if (data.length === 0){
-            // respond with error message saying there are no users with lastname
-            res.status(404).json({
-                message:`No users with username: ${req.params.username}`
-            });
-        // if there are users in the array from query.
-        } else {
-            // respond with data from SQL query
-            res.status(200).json("User Delted Successfully")
-        }
-    })
-    // if there is an error retrieving from database: log error in console and send response back with error and errorMessage.
-    .catch( error => {
-        console.log( error );
-        res.status(500).json({
-            error:error,
-            errorMessage:'Error removing user'
-        })
-    })
-})
+// router.delete('/:username', (req, res)=>{
+//     userDb.removeUser( req.params.username )
+//     // if the SQL query is successful 
+//     .then(data=>{
+//         // if the length of the array from the SQL query is 0
+//         if (data.length === 0){
+//             // respond with error message saying there are no users with lastname
+//             res.status(404).json({
+//                 message:`No users with username: ${req.params.username}`
+//             });
+//         // if there are users in the array from query.
+//         } else {
+//             // respond with data from SQL query
+//             res.status(200).json("User Delted Successfully")
+//         }
+//     })
+//     // if there is an error retrieving from database: log error in console and send response back with error and errorMessage.
+//     .catch( error => {
+//         console.log( error );
+//         res.status(500).json({
+//             error:error,
+//             errorMessage:'Error removing user'
+//         })
+//     })
+// })
 
 
 // function to create a token for successful login. takes in user object.
