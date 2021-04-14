c7573a64beeccd9b6dd89c01404272e47378640f
Marcellino Fernandez <munch2000@gmail.com>
2021-04-14 18:56:03 -0400

    actually merged old branch with new changes. last commit was a mistake

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,7 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString: process.env.DATABASE_URL,
-<<<<<<< HEAD
       ssl: { rejectUnauthorized: false},
-=======
-      ssl: { rejectedUnauthorized: false }
->>>>>>> 8188b282cd44da83be62d901ff5fa2a19a0d2521
     },
     migrations: { directory: "./data/migrations" },
     seeds: { directory: "./data/seeds" }
